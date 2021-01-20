5f746a82067f34cffd31ce153a1bd4b95cd57abd
Muhammad Hilmi Al Fatih <hilmialf@gmail.com>
2021-01-20 18:32:23 +0900

    fix sanctum config issue

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,8 +15,8 @@ return [
 
     'stateful' => explode(',', env(
         'SANCTUM_STATEFUL_DOMAINS',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1'
-    ) . env('APP_URL') . env('CORS_ALLOWED_ORIGIN')),
+        'localhost:3000, spa-starter-vue2.gruneasia2.info/, spa-starter-vue2.gruneasia2.info'
+    )),
 
     /*
     |--------------------------------------------------------------------------
@@ -43,8 +43,8 @@ return [
     */
 
     'middleware' => [
-        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
-        'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
+        'verify_csrf_token' => \App\Http\Middleware\VerifyCsrfToken::class,
+        'encrypt_cookies' => \App\Http\Middleware\EncryptCookies::class,
     ],
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -17,51 +17,52 @@ use Illuminate\Support\Facades\Auth;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
-
 Route::get('/sanctum/csrf-cookie', function (Request $request) {
     /*
     * Set xsrf cookie for login
     */
     return response()->json();
 });
-
 Route::get('/auth-check', 'Auth\LoginController@checkIsAuthenticated');
 Route::post('/login', 'Auth\LoginController@login');
 Route::get('/logout', 'Auth\LoginController@logout');
 
-Route::get('/users', 'Api\UserController@getAll');
-Route::get('/users/create', 'Api\UserController@create');
-Route::post('/users', 'Api\UserController@store');
-Route::get('/users/{userId}', 'Api\UserController@show');
-Route::get('/users/{userId}/edit', 'Api\UserController@edit');
-Route::post('/users/{userId}', 'Api\UserController@update');
-Route::delete('/users/{userId}', 'Api\UserController@destroy');
-
-// DUMMY ROUTE //
-Route::get('/dummy-meetings', 'Api\DummyMeetingController@getAll');
-Route::get('/dummy-meetings/create', 'Api\DummyMeetingController@create');
-Route::post('/dummy-meetings', 'Api\DummyMeetingController@store');
-Route::get('/dummy-meetings/{meetingId}', 'Api\DummyMeetingController@show');
-Route::get('/dummy-meetings/{meetingId}/edit', 'Api\DummyMeetingController@edit');
-Route::post('/dummy-meetings/{meetingId}', 'Api\DummyMeetingController@update');
-Route::delete('/dummy-meetings/{meetingId}', 'Api\DummyMeetingController@destroy');
-// --------------- //
+Route::middleware(['auth:sanctum'])->group(function () {
 
 
+    Route::get('/users', 'Api\UserController@getAll');
+    Route::get('/users/create', 'Api\UserController@create');
+    Route::post('/users', 'Api\UserController@store');
+    Route::get('/users/{userId}', 'Api\UserController@show');
+    Route::get('/users/{userId}/edit', 'Api\UserController@edit');
+    Route::post('/users/{userId}', 'Api\UserController@update');
+    Route::delete('/users/{userId}', 'Api\UserController@destroy');
 
-Route::get('/hello', function (Request $request) {
-    $res = Auth::check();
-    $minutes = 60;
-    $path = "";
-    $domain = "localhost";
-    $response =  response()
-        ->json($res)
-        ->cookie('testCookie2', 'testCookie2', $minutes, $path, $domain)
-        ->withHeaders([
-            'X-Header-One' => 'Header Value',
-            'X-Header-Two' => 'Header Value',
-        ]);
-    // dd($response);
-    return $response;
+    // DUMMY ROUTE //
+    Route::get('/dummy-meetings', 'Api\DummyMeetingController@getAll');
+    Route::get('/dummy-meetings/create', 'Api\DummyMeetingController@create');
+    Route::post('/dummy-meetings', 'Api\DummyMeetingController@store');
+    Route::get('/dummy-meetings/{meetingId}', 'Api\DummyMeetingController@show');
+    Route::get('/dummy-meetings/{meetingId}/edit', 'Api\DummyMeetingController@edit');
+    Route::post('/dummy-meetings/{meetingId}', 'Api\DummyMeetingController@update');
+    Route::delete('/dummy-meetings/{meetingId}', 'Api\DummyMeetingController@destroy');
+    // --------------- //
+
+
+
+    Route::get('/hello', function (Request $request) {
+        $res = Auth::check();
+        $minutes = 60;
+        $path = "";
+        $domain = "localhost";
+        $response =  response()
+            ->json($res)
+            ->cookie('testCookie2', 'testCookie2', $minutes, $path, $domain)
+            ->withHeaders([
+                'X-Header-One' => 'Header Value',
+                'X-Header-Two' => 'Header Value',
+            ]);
+        // dd($response);
+        return $response;
+    });
 });

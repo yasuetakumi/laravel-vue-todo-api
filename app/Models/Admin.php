a1c0b51ce11fa204d1316ec94a0dfbc7bc1b418d
Muhammad Hilmi Al Fatih <hilmialf@gmail.com>
2021-02-13 09:06:46 +0900

    fix admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Admin extends Model {
+class Admin extends Authenticatable {
     use Notifiable, SoftDeletes;
 
     protected $fillable = [

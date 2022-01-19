429935e60c2448232d81734fd72a0cb92ead6852
adiyoga <adi.yoga@gruneasia.info>
2022-01-19 18:06:29 +0800

    [ADT-JAN-8]Update Customer list page

--- database/seeds/CustomerSeeder.php
+++ database/seeds/CustomerSeeder.php
@@ -13,14 +13,54 @@ class CustomerSeeder extends Seeder {
     public function run() {
         // ---------------------------------------------------------------------
         $customers = [
-            ['name' => 'Amazon'],
-            ['name' => 'Google'],
-            ['name' => 'Facebook'],
-            ['name' => 'Apple'],
-            ['name' => 'Netflix'],
-            ['name' => 'LinkedIn'],
-            ['name' => 'Docomo'],
-            ['name' => 'Microsoft'],
+            [
+                'name'      => 'Amazon', 
+                'email'     => 'email@amazon.com',
+                'phone'     => '813-4540-5951',
+                'website'   => 'amazon.com',
+            ],
+            [
+                'name'      => 'Google',
+                'email'     => 'email@google.com',
+                'phone'     => '812-3456-7891',
+                'website'   => 'google.com',
+            ],
+            [
+                'name'      => 'Facebook',
+                'email'     => 'email@facebook.com',
+                'phone'     => '812-3456-7891',
+                'website'   => 'facebook.com',
+            ],
+            [
+                'name'      => 'Apple',
+                'email'     => 'email@apple.com',
+                'phone'     => '812-3456-7891',
+                'website'   => 'apple.com',
+            ],
+            [
+                'name'      => 'Netflix',
+                'email'     => 'email@netflix.com',
+                'phone'     => '812-3456-7891',
+                'website'   => 'netflix.com',
+            ],
+            [
+                'name'      => 'LinkedIn',
+                'email'     => 'email@linkedin.com',
+                'phone'     => '812-3456-7891',
+                'website'   => 'linkedin.com',
+            ],
+            [
+                'name'      => 'Docomo',
+                'email'     => 'email@nttdocomo.com',
+                'phone'     => '812-3456-7891',
+                'website'   => 'nttdocomo.co.jp',
+            ],
+            [
+                'name'      => 'Microsoft',
+                'email'     => 'email@microsoft.com',
+                'phone'     => '812-3456-7891',
+                'website'   => 'microsoft.com',
+            ],
         ];
         // ---------------------------------------------------------------------
         foreach ($customers as $key => $customer) {

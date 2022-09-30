2ddee2c2d54bde759fa99f3cd8539d356122b19d
yasuetakumi <t-yasue@grune.co.jp>
2022-09-30 13:29:06 +0900

    corsエラー

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
+Route::group(['middleware' => 'cors'], function () {
+
+    /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
@@ -14,106 +16,107 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-// ---------------------------------------------------------------------
-// Route for Mobile (Flutter Starter-Kit)
-// ---------------------------------------------------------------------
-Route::post('/sanctum/token', 'Auth\MobileLoginController@makeToken');
-// ---------------------------------------------------------------------
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-Route::get('/sanctum/csrf-cookie', function (Request $request) {
-    /*
-    * Set xsrf cookie for login
-    */
-    return response()->json();
-});
-Route::get('/auth-check', 'Auth\LoginController@checkIsAuthenticated');
-Route::post('/login', 'Auth\LoginController@login');
-Route::get('/logout', 'Auth\LoginController@logout');
-
-Route::get('/hand-shake', function(){
-    return response()->json(["result"=>"___SUCCESS___"]);
-});
-
-/*
-* In case we want different user provider for our session,
-* we can use the following endpoints.
-*/
-Route::post('/{guard}/login', 'Auth\LoginController@login');
-Route::get('/{guard}/logout', 'Auth\LoginController@logout');
-
-
-Route::middleware(['auth:web,admin,sanctum'])->group(function () {
-
-    Route::get('/todo/list', 'Api\TodoController@index');
-    Route::post('/todo/store', 'Api\TodoController@store');
-    Route::post('/todo/update', 'Api\TodoController@update');
-    Route::post('/todo/delete', 'Api\TodoController@delete');
-
     // ---------------------------------------------------------------------
     // Route for Mobile (Flutter Starter-Kit)
     // ---------------------------------------------------------------------
-    Route::get('/user/revoke', 'Auth\MobileLoginController@revokeToken');
-    Route::get('/login-user', 'Auth\MobileLoginController@loginUser');
+    Route::post('/sanctum/token', 'Auth\MobileLoginController@makeToken');
     // ---------------------------------------------------------------------
 
-    Route::get('/users', 'Api\UserController@getAll');
-    Route::get('/users/downloadCSV', 'Api\UserController@downloadCSV');
-    Route::get('/users/create', 'Api\UserController@create');
-    Route::post('/users', 'Api\UserController@store');
-    // --- import user from CSV
-    Route::post('/users/import-csv', 'Api\UserController@importCsv');
-    // --- END import user from CSV
-    Route::get('/users/{userId}', 'Api\UserController@show');
-    Route::get('/users/{userId}/edit', 'Api\UserController@edit');
-    Route::post('/users/{userId}', 'Api\UserController@update');
-    Route::delete('/users/{userId}', 'Api\UserController@destroy');
+    // Route::middleware('auth:api')->get('/user', function (Request $request) {
+    //     return $request->user();
+    // });
+    Route::get('/sanctum/csrf-cookie', function (Request $request) {
+        /*
+    * Set xsrf cookie for login
+    */
+        return response()->json();
+    });
+    Route::get('/auth-check', 'Auth\LoginController@checkIsAuthenticated');
+    Route::post('/login', 'Auth\LoginController@login');
+    Route::get('/logout', 'Auth\LoginController@logout');
 
-    // DUMMY ROUTE //
-    // Route::get('/dummy-meetings', 'Api\DummyMeetingController@getAll');
-    // Route::get('/dummy-meetings/create', 'Api\DummyMeetingController@create');
-    // Route::post('/dummy-meetings', 'Api\DummyMeetingController@store');
-    // Route::get('/dummy-meetings/{meetingId}', 'Api\DummyMeetingController@show');
-    // Route::get('/dummy-meetings/{meetingId}/edit', 'Api\DummyMeetingController@edit');
-    // Route::post('/dummy-meetings/{meetingId}', 'Api\DummyMeetingController@update');
-    // Route::delete('/dummy-meetings/{meetingId}', 'Api\DummyMeetingController@destroy');
-    // meetings route
-    Route::get('/meetings', 'Api\DummyMeetingController@getAll');
-    Route::get('/meetings/create', 'Api\DummyMeetingController@create');
-    Route::post('/meetings', 'Api\DummyMeetingController@store');
-    Route::get('/meetings/{meetingId}', 'Api\DummyMeetingController@show');
-    Route::get('/meetings/{meetingId}/edit', 'Api\DummyMeetingController@edit');
-    Route::post('/meetings/{meetingId}', 'Api\DummyMeetingController@update');
-    Route::delete('/meetings/{meetingId}', 'Api\DummyMeetingController@destroy');
-    Route::get('/image/{filename}', 'Api\DummyMeetingController@displayImage')->name('image.displayImage');
-    // --------------- //
+    Route::get('/hand-shake', function () {
+        return response()->json(["result" => "___SUCCESS___"]);
+    });
+
+    /*
+* In case we want different user provider for our session,
+* we can use the following endpoints.
+*/
+    Route::post('/{guard}/login', 'Auth\LoginController@login');
+    Route::get('/{guard}/logout', 'Auth\LoginController@logout');
 
 
-    Route::get('/customers', 'Api\CustomerController@getAll');
-    Route::get('/customers/create', 'Api\CustomerController@create');
-    Route::post('/customers', 'Api\CustomerController@store');
-    Route::get('/customers/{customerId}', 'Api\CustomerController@show');
-    Route::get('/customers/{customerId}/edit', 'Api\CustomerController@edit');
-    Route::post('/customers/{customerId}', 'Api\CustomerController@update');
-    Route::delete('/customers/{customerId}', 'Api\CustomerController@destroy');
+    Route::middleware(['auth:web,admin,sanctum'])->group(function () {
 
-    Route::get('/postcode/{postcode}', 'Api\PostcodeController@address')->name('v1.api.postcode');
+        Route::get('/todo/list', 'Api\TodoController@index');
+        Route::post('/todo/store', 'Api\TodoController@store');
+        Route::post('/todo/update', 'Api\TodoController@update');
+        Route::post('/todo/delete', 'Api\TodoController@delete');
 
-    Route::get('/hello', function (Request $request) {
-        $res = Auth::check();
-        $minutes = 60;
-        $path = "";
-        $domain = "localhost";
-        $response =  response()
-            ->json($res)
-            ->cookie('testCookie2', 'testCookie2', $minutes, $path, $domain)
-            ->withHeaders([
-                'X-Header-One' => 'Header Value',
-                'X-Header-Two' => 'Header Value',
-            ]);
-        // dd($response);
-        return $response;
+        // ---------------------------------------------------------------------
+        // Route for Mobile (Flutter Starter-Kit)
+        // ---------------------------------------------------------------------
+        Route::get('/user/revoke', 'Auth\MobileLoginController@revokeToken');
+        Route::get('/login-user', 'Auth\MobileLoginController@loginUser');
+        // ---------------------------------------------------------------------
+
+        Route::get('/users', 'Api\UserController@getAll');
+        Route::get('/users/downloadCSV', 'Api\UserController@downloadCSV');
+        Route::get('/users/create', 'Api\UserController@create');
+        Route::post('/users', 'Api\UserController@store');
+        // --- import user from CSV
+        Route::post('/users/import-csv', 'Api\UserController@importCsv');
+        // --- END import user from CSV
+        Route::get('/users/{userId}', 'Api\UserController@show');
+        Route::get('/users/{userId}/edit', 'Api\UserController@edit');
+        Route::post('/users/{userId}', 'Api\UserController@update');
+        Route::delete('/users/{userId}', 'Api\UserController@destroy');
+
+        // DUMMY ROUTE //
+        // Route::get('/dummy-meetings', 'Api\DummyMeetingController@getAll');
+        // Route::get('/dummy-meetings/create', 'Api\DummyMeetingController@create');
+        // Route::post('/dummy-meetings', 'Api\DummyMeetingController@store');
+        // Route::get('/dummy-meetings/{meetingId}', 'Api\DummyMeetingController@show');
+        // Route::get('/dummy-meetings/{meetingId}/edit', 'Api\DummyMeetingController@edit');
+        // Route::post('/dummy-meetings/{meetingId}', 'Api\DummyMeetingController@update');
+        // Route::delete('/dummy-meetings/{meetingId}', 'Api\DummyMeetingController@destroy');
+        // meetings route
+        Route::get('/meetings', 'Api\DummyMeetingController@getAll');
+        Route::get('/meetings/create', 'Api\DummyMeetingController@create');
+        Route::post('/meetings', 'Api\DummyMeetingController@store');
+        Route::get('/meetings/{meetingId}', 'Api\DummyMeetingController@show');
+        Route::get('/meetings/{meetingId}/edit', 'Api\DummyMeetingController@edit');
+        Route::post('/meetings/{meetingId}', 'Api\DummyMeetingController@update');
+        Route::delete('/meetings/{meetingId}', 'Api\DummyMeetingController@destroy');
+        Route::get('/image/{filename}', 'Api\DummyMeetingController@displayImage')->name('image.displayImage');
+        // --------------- //
+
+
+        Route::get('/customers', 'Api\CustomerController@getAll');
+        Route::get('/customers/create', 'Api\CustomerController@create');
+        Route::post('/customers', 'Api\CustomerController@store');
+        Route::get('/customers/{customerId}', 'Api\CustomerController@show');
+        Route::get('/customers/{customerId}/edit', 'Api\CustomerController@edit');
+        Route::post('/customers/{customerId}', 'Api\CustomerController@update');
+        Route::delete('/customers/{customerId}', 'Api\CustomerController@destroy');
+
+        Route::get('/postcode/{postcode}', 'Api\PostcodeController@address')->name('v1.api.postcode');
+
+        Route::get('/hello', function (Request $request) {
+            $res = Auth::check();
+            $minutes = 60;
+            $path = "";
+            $domain = "localhost";
+            $response =  response()
+                ->json($res)
+                ->cookie('testCookie2', 'testCookie2', $minutes, $path, $domain)
+                ->withHeaders([
+                    'X-Header-One' => 'Header Value',
+                    'X-Header-Two' => 'Header Value',
+                ]);
+            // dd($response);
+            return $response;
+        });
     });
 });

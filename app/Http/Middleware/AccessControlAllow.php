d333573e651bf1ac97adbcea3d6c2e9f49a9fcee
yasuetakumi <t-yasue@grune.co.jp>
2022-09-30 10:51:18 +0900

    cors middleware add

---
+++ app/Http/Middleware/AccessControlAllow.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AccessControlAllow
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'https://study-vue.com') // 「Vueの開発用サーバー」に合わせる。
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH')
+            ->header('Access-Control-Allow-Headers', 'Content-Type');
+    }
+}

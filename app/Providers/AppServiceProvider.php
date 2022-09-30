aec285118b412cf8690df236bac2ecf402404759
yasuetakumi <t-yasue@grune.co.jp>
2022-09-30 15:10:31 +0900

    デプロイ

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,8 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (App::environment('production')) {
-            URL::forceScheme('https');
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
         }
 
         Schema::defaultStringLength(191);
